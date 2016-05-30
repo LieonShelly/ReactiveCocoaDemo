6ad7c6cf8eb0241d0a49fdec3f84ceaea525278e
Demo <apple@appledeMacBook-Pro.local>
2016-05-30 13:50:46 +0800

    导入ReactiveCocoa框架

---
+++ ReacttiveCocoaDemo/ReacttiveCocoaDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReacttiveCocoaDemo
+//
+//  Created by apple on 16/5/30.
+//  Copyright © 2016年 lieon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
